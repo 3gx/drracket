361e9f59618e1c849488532f16347a16446f72c4
egaburov <eg0x52@gmail.com>
2019-07-13 15:32:55 -0700

    working on implementing big nums

--- EOPL3/ch2a.rkt
+++ EOPL3/ch2a.rkt
@@ -5,6 +5,10 @@
   (define is-zero? (lambda (n) (null? n)))
   (define successor (lambda (n) (cons #t n)))
   (define predecessor (lambda (n) (cdr n)))
+  (define (value n)
+    (if (is-zero? n)
+      0
+      (add1 (value (cdr n)))))
   (provide (all-defined-out))
 )
 
@@ -13,11 +17,24 @@
   (define is-zero? (lambda (n) (zero? n)))
   (define successor (lambda (n) (add1 n)))
   (define predecessor (lambda (n) (sub1 n)))
+  (define (value n) n)
   (provide (all-defined-out))
 )
 
 (module v3 racket
-  (define zero (lambda () '(0)))
+  (define NN 16)
+  (define zero (lambda () '()))
+  (define (iz-zero? n) (lambda (n) (null? n)))
+  (define (inc n)
+    (if (< (add1 n) NN)
+          (cons 0 (add1 n))
+          (cons 1 0)))
+  (define (dec n)
+    (if (< (sub1 n) 0)
+          (cons -1 0)
+          (cons 0 (sub1 n))))
+  (define (successor n)
+    'not-implemeted)
   (provide (all-defined-out))
 )
 
@@ -41,3 +58,4 @@ two
 four
 (define eight (mul two four))
 eight
+(println (format "value: ~a" (value eight)))
