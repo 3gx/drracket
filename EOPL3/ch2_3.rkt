013fbd91719dcc6800fb022cdadbaf2423e9af09
egaburov <eg0x52@gmail.com>
2019-07-19 08:47:08 -0700

    saved

--- EOPL3/ch2_3.rkt
+++ EOPL3/ch2_3.rkt
@@ -5,9 +5,31 @@
     [(var-exp? exp) (eqv? var (var-exp->var exp))]
     [(lambda-exp? exp)
      (and
-       (not (eqv? var (lambda-expr->boudn-var exp)))
+       (not (eqv? var (lambda-expr->bound-var exp)))
        (occurs-free? var (lambda-expr->body exp)))]
     [else
       (or
         (occurs-free? var (app-exp->rator exp))
         (occurs-free? var (app-exp->rand exp)))]))
+
+(define (var-exp? exp)
+  (symbol? exp))
+(define (var-exp->var exp)
+  exp)
+(define (lambda-exp? exp)
+  (eqv? (car exp) 'lambda))
+(define (lambda-expr->bound-var exp)
+  (caadr exp))
+(define (lambda-expr->body exp)
+  (caddr exp))
+(define (app-exp->rator exp)
+  (car exp))
+(define (app-exp->rand exp)
+  (cadr exp))
+
+(occurs-free? 'x 'x)
+(occurs-free? 'x 'y)
+(occurs-free? 'x '(lambda (x) (x y)))
+(occurs-free? 'x '(lambda (y) (x y)))
+(occurs-free? 'x '((lambda (x) x) (x y)))
+
