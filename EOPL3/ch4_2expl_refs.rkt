a6c31ad2df6e3e25729297a37f787f270fe73842
egaburov <eg0x52@gmail.com>
2019-07-21 18:44:13 -0700

    works

--- EOPL3/ch4_2expl_refs.rkt
+++ EOPL3/ch4_2expl_refs.rkt
@@ -91,36 +91,6 @@
 
 ;;;;;;;;;;;;
 
-(define prog1 (scan&parse "
-  let x = newref(0)
-  in letrec even(dummy)
-              = if zero?(deref(x))
-                then 1
-                else begin
-                      setref(x, -(deref(x),1));
-                      (odd 888)
-                     end
-            odd(dummy)
-             = if zero?(deref(x))
-               then 0
-               else begin
-                       setref(x, -(deref(x),1));
-                       (even 888)
-                    end
-      in begin setref(x,13); (odd 888) end
-                          "))
-prog1
-
-(define prog2 (scan&parse "
-      let g = let counter = newref(0)
-              in proc (dummy) begin
-                    setref(counter, -(deref(counter), -1)); deref(counter)
-                end
-      in let a = (g 11)
-         in let b = (g 11)
-            in -(a,b)
-"))
-prog2
 
 (define (init-env)
   (extend-env
@@ -340,7 +310,38 @@ the-store
 (deref r2)
 |#
 
+
+(define prog2 (scan&parse "
+      let g = let counter = newref(0)
+              in proc (dummy) begin
+                    setref(counter, -(deref(counter), -1)); deref(counter)
+                end
+      in let a = (g 11)
+         in let b = (g 11)
+            in -(a,b)
+"))
+prog2
 (value-of-program prog2)
+
+(define prog1 (scan&parse "
+  let x = newref(0)
+  in letrec even(dummy)
+              = if zero?(deref(x))
+                then 1
+                else begin
+                      setref(x, -(deref(x),1));
+                      (odd 888)
+                     end
+            odd(dummy)
+             = if zero?(deref(x))
+               then 0
+               else begin
+                       setref(x, -(deref(x),1));
+                       (even 888)
+                    end
+      in begin setref(x,13); (odd 888) end
+                          "))
+prog1
 (value-of-program prog1)
 
 
