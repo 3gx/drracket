413f7229661682b9c0486b83bf8e4aea9a6a1712
egaburov <eg0x52@gmail.com>
2019-07-23 07:40:46 -0700

    letrec cont

---
+++ EOPL3/ch5_1letrec.rkt
@@ -0,0 +1,273 @@
+#lang racket
+
+(require eopl)
+
+
+;;;;;;;;;;;;;;;; grammatical specification ;;;;;;;;;;;;;;;;
+
+(define the-lexical-spec
+  '((whitespace (whitespace) skip)
+    (comment ("%" (arbno (not #\newline))) skip)
+    (identifier
+     (letter (arbno (or letter digit "_" "-" "?")))
+     symbol)
+    (number (digit (arbno digit)) number)
+    (number ("-" digit (arbno digit)) number)
+    ))
+
+(define the-grammar
+  '((program (expression) a-program)
+
+    (expression (number) const-exp)
+    (expression
+     ("-" "(" expression "," expression ")")
+     diff-exp)
+    (expression
+     ("+" "(" expression "," expression ")")
+     plus-exp)
+    (expression
+     ("*" "(" expression "," expression ")")
+     mul-exp)
+
+    (expression
+     ("zero?" "(" expression ")")
+     zero?-exp)
+
+    (expression
+     ("if" expression "then" expression "else" expression)
+     if-exp)
+
+    (expression (identifier) var-exp)
+
+    (expression
+     ("let" identifier "=" expression "in" expression)
+     let-exp)
+
+    (expression
+      ("proc" "(" identifier ")" expression)
+      proc-exp)
+
+    (expression
+      ("(" expression expression ")")
+      call-exp)
+
+    (expression
+      ("letrec"
+       identifier "(" identifier ")" "=" expression
+       "in" expression)
+      letrec-exp)
+
+    ))
+
+;;;;;;;;;;;;;;;; sllgen boilerplate ;;;;;;;;;;;;;;;;
+
+(sllgen:make-define-datatypes the-lexical-spec the-grammar)
+
+(define show-the-datatypes
+  (lambda () (sllgen:list-define-datatypes the-lexical-spec the-grammar)))
+
+(define scan&parse
+  (sllgen:make-string-parser the-lexical-spec the-grammar))
+
+(define just-scan
+  (sllgen:make-string-scanner the-lexical-spec the-grammar))
+
+
+
+(scan&parse "-(55, -(x,11))")
+(scan&parse "if 42 then 43 else 45")
+(scan&parse "
+  let f = proc(x) -(x,11)
+  in (f (f 77))
+  ")
+(scan&parse "
+  let f = proc(x) -(x,11)
+  in (f (f 77))
+  ")
+(scan&parse "
+  let x = 200
+  in let f = proc (z) -(z,x)
+     in let x = 100
+        in let g = proc (z) -(z,x)
+           in -((f 1), (g 1))
+           ")
+
+(define-datatype environment environment?
+  (empty-env)
+  (extend-env
+    (var symbol?)
+    (val expval?)
+    (env environment?))
+  (extend-env-rec
+    (p-name symbol?)
+    (b-var symbol?)
+    (body expression?)
+    (env environment?)))
+(define (apply-env env search-var)
+  (cases environment env
+    (empty-env ()
+      (error "No binding for" search-var))
+    (extend-env (saved-var saved-val saved-env)
+      (if (eqv? saved-var search-var)
+        saved-val
+        (apply-env saved-env search-var)))
+    (extend-env-rec (p-name b-var p-body saved-env)
+      (if (eqv? p-name search-var)
+        (proc-val (procedure b-var p-body env))
+        (apply-env saved-env search-var)))))
+
+(define (init-env)
+  (extend-env
+    'i (num-val 1)
+    (extend-env
+      'v (num-val 5)
+      (extend-env
+        'x (num-val 10)
+        (empty-env)))))
+
+(define-datatype proc proc?
+  (procedure
+    (var symbol?)
+    (body expression?)
+    (env environment?)))
+
+
+
+
+(define-datatype expval expval?
+  (num-val
+    (value number?))
+  (bool-val
+    (boolean boolean?))
+  (proc-val
+    (proc proc?)))
+
+(define (expval->num val)
+  (cases expval val
+    (num-val (num) num)
+    (else (error "failed to extract num" val))))
+
+(define (expval->bool val)
+  (cases expval val
+    (bool-val (bool) bool)
+    (else (error "failed to extract bool" val))))
+
+(define (apply-procedure proc1 val)
+  (cases proc proc1
+    (procedure (var body env)
+      (value-of body (extend-env var val env)))))
+
+(define (expval->proc val)
+  (cases expval val
+    (proc-val (proc) proc)
+    (else (error "failed to extract proc" val))))
+
+(define (run ast)
+  (value-of-program ast))
+
+(define (value-of-program pgm)
+  (cases program pgm
+    (a-program (exp1)
+      (value-of exp1 (init-env)))))
+
+(define (value-of exp env)
+  (cases expression exp
+    (const-exp (num) (num-val num))
+    (var-exp (var) (apply-env env var))
+    (diff-exp (exp1 exp2)
+      (let* ([val1 (value-of exp1 env)]
+             [val2 (value-of exp2 env)]
+             [num1 (expval->num val1)]
+             [num2 (expval->num val2)])
+        (num-val (- num1 num2))))
+    (plus-exp (exp1 exp2)
+      (let* ([val1 (value-of exp1 env)]
+             [val2 (value-of exp2 env)]
+             [num1 (expval->num val1)]
+             [num2 (expval->num val2)])
+        (num-val (+ num1 num2))))
+    (mul-exp (exp1 exp2)
+      (let* ([val1 (value-of exp1 env)]
+             [val2 (value-of exp2 env)]
+             [num1 (expval->num val1)]
+             [num2 (expval->num val2)])
+        (num-val (* num1 num2))))
+    (zero?-exp (exp1)
+      (let* ([val1 (value-of exp1 env)]
+             [num1 (expval->num val1)])
+        (if (zero? num1)
+          (bool-val #t)
+          (bool-val #f))))
+    (if-exp (exp1 exp2 exp3)
+      (let ([val1 (value-of exp1 env)])
+        (if (expval->bool val1)
+          (value-of exp2 env)
+          (value-of exp3 env))))
+    (let-exp (var exp1 body)
+      (let ([val1 (value-of exp1 env)])
+        (value-of body (extend-env var val1 env))))
+    (proc-exp (var body)
+      (proc-val (procedure var body env)))
+    (call-exp (rator rand)
+      (let ([proc (expval->proc (value-of rator env))]
+            [arg (value-of rand env)])
+        (apply-procedure proc arg)))
+    (letrec-exp (p-name b-var p-body letrec-body)
+      (value-of letrec-body (extend-env-rec p-name b-var p-body env)))))
+
+
+
+(define ast1
+  (scan&parse "
+    letrec double(x)
+       = if zero?(x) then 0 else -((double -(x,1)), -2)
+    in (double 6)
+    "))
+ast1
+(run ast1)
+
+(define ast2
+  (scan&parse "
+      letrec even(x) =
+          if zero?(x) then 1 else (odd -(x,1))
+      in letrec odd(x) =
+            if zero?(x) then 0 else (even -(x,1))
+         in (odd 13)
+  "))
+ast2
+;(run ast2)
+
+(define ast3
+  (scan&parse "
+    letrec fact(n) =
+       if zero?(n) then 1 else *(n, (fact -(n,1)))
+    in (fact 5)
+  "))
+ast3
+(run ast3)
+
+(define ast4
+  (scan&parse "
+    letrec fix(f) =
+      letrec d(x) = proc(z) ((f (x x)) z)
+      in proc(n) ((f (d d)) n)
+    in letrec t4m(f) =
+          proc(x) if zero?(x) then 0 else +((f -(x,1)),4)
+       in let times4 = (fix t4m)
+          in (times4 3)"
+     ))
+ast4
+(run ast4)
+
+(define ast4a
+  (scan&parse "
+    letrec fix(f) =
+      letrec d(x) = (f (x x))
+      in (f (d d))
+    in letrec t4m(f) =
+          proc(x) if zero?(x) then 0 else +((f -(x,1)),4)
+       in let times4 = (fix t4m)
+          in (times4 3)"
+     ))
+ast4a
+;(run ast4a) - runs infinitely
